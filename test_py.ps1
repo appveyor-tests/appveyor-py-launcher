9aed55ac37b7c124724e2a8f865357ad921fd352
Feodor Fitsner <feodor@appveyor.com>
2017-10-19 09:41:19 -0700

    Create test_py.ps1

---
+++ test_py.ps1
@@ -0,0 +1,24 @@
+$ErrorActionPreference = "Stop"
+
+$result = (cmd /c info.py)
+$result
+if ($result[0] -ne '>>> import sys') { throw "Unexpected result from launching info.py" }
+
+$py_versions = @(
+    "2.6"
+    "2.6-32"
+    "2.7"
+    "2.7-32"
+    "3.3"
+    "3.3-32"
+    "3.4"
+    "3.4-32"
+    "3.5"
+    "3.5-32"
+    "3.6"
+    "3.6-32"
+)
+
+foreach($py_version in $py_versions) {
+    py.exe -$py_version .\info.py
+}
